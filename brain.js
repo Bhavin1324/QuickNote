0bb011ae139d5bf9056e8ab904bc536a19cf6173
bj <jariwala24b@gmail.com>
2022-01-22 16:45:56 +0530

    Implemented Theme configuration

--- brain.js
+++ brain.js
@@ -15,7 +15,6 @@ window.addEventListener('resize', () => {
     }
     let x = window.matchMedia("(min-width:992px)");
     // let x1 = window.matchMedia("(min-width:)");
-    console.log(x);
     widthCheck(x); // invoking runtime
     x.addListener(widthCheck); //set function on state change
 })
@@ -85,5 +84,60 @@ function widthCheck(x) {
 
 // ==================== for taking in work ==================== //
 
+// Themes 
+// light mode
+let light = document.querySelector('.dropdown-content a').nextElementSibling;
+light.addEventListener('click',()=>{
+    document.querySelector('.navbar').classList.remove('navbar-dark');
+    document.getElementsByTagName('body')[0].classList.remove('backdark');
+    document.querySelector('.divider').classList.remove('divider-dark');
+    document.querySelector('.startup').classList.remove('startup-dark');
+    Array.from(document.querySelectorAll('.card')).forEach((e)=>e.classList.remove('card-dark'));
+    Array.from(document.querySelectorAll('.inpbox')).forEach((e)=>e.classList.remove('inpbox-dark'));
+    document.querySelector('.dropdown-content').classList.remove('drop-cont-dark');
+
+    document.querySelector('.navbar').classList.add('navbar-light');
+    Array.from(document.querySelectorAll('.line')).forEach((e)=>e.classList.add('line-light'));
+    document.querySelector('.search button').classList.add('btn-dark');
+    document.querySelector('.hamburger').classList.add('hamburger-light');
+    Array.from(document.querySelectorAll('.burgerslice')).forEach((e)=>e.classList.add('burgerslice-light'));
+    document.querySelector('.dropdown-content').classList.add('drop-cont-light');
+});
+
+// drak mode
+let dark = document.querySelector('.dropdown-content a');
+dark.addEventListener('click',()=>{
+    document.querySelector('.navbar').classList.remove('navbar-light');
+    Array.from(document.querySelectorAll('.line')).forEach((e)=>e.classList.remove('line-light'));
+    document.querySelector('.search button').classList.remove('btn-dark');
+    document.querySelector('.hamburger').classList.remove('hamburger-light');
+    Array.from(document.querySelectorAll('.burgerslice')).forEach((e)=>e.classList.remove('burgerslice-light'));
+    document.querySelector('.dropdown-content').classList.remove('drop-cont-light');
+
+    document.querySelector('.navbar').classList.add('navbar-dark');
+    document.getElementsByTagName('body')[0].classList.add('backdark');
+    document.querySelector('.divider').classList.add('divider-dark');
+    document.querySelector('.startup').classList.add('startup-dark');
+    Array.from(document.querySelectorAll('.card')).forEach((e)=>e.classList.add('card-dark'));
+    Array.from(document.querySelectorAll('.inpbox')).forEach((e)=>e.classList.add('inpbox-dark'));
+    document.querySelector('.dropdown-content').classList.add('drop-cont-dark');
+});
+//default mode
+let sysdef =  document.querySelector('.dropdown-content a').nextElementSibling.nextElementSibling;
+sysdef.addEventListener('click',()=>{
+    document.querySelector('.navbar').classList.remove('navbar-dark');
+    document.getElementsByTagName('body')[0].classList.remove('backdark');
+    document.querySelector('.divider').classList.remove('divider-dark');
+    document.querySelector('.startup').classList.remove('startup-dark');
+    Array.from(document.querySelectorAll('.card')).forEach((e)=>e.classList.remove('card-dark'));
+    Array.from(document.querySelectorAll('.inpbox')).forEach((e)=>e.classList.remove('inpbox-dark'));
+    document.querySelector('.dropdown-content').classList.remove('drop-cont-dark');
+    document.querySelector('.navbar').classList.remove('navbar-light');
+    Array.from(document.querySelectorAll('.line')).forEach((e)=>e.classList.remove('line-light'));
+    document.querySelector('.search button').classList.remove('btn-dark');
+    document.querySelector('.hamburger').classList.remove('hamburger-light');
+    Array.from(document.querySelectorAll('.burgerslice')).forEach((e)=>e.classList.remove('burgerslice-light'));
+    document.querySelector('.dropdown-content').classList.remove('drop-cont-light');
+})
 
 
