357e93426101847d3d49302ed4898204fb2cf3a0
bj <jariwala24b@gmail.com>
2022-01-22 08:58:28 +0530

    Bug fixed

--- brain.js
+++ brain.js
@@ -1,6 +1,27 @@
 let ham = document.querySelector('.hamburger');
 let lis = document.querySelectorAll('.list-item');
 let btn = document.querySelector('.btn');
+window.addEventListener('resize',()=>{
+    if(window.innerWidth >= 992){
+        lis.forEach((e)=>{e.style.display = 'block'});
+        document.querySelector('.searchpack').style.display = 'block';
+    }
+    else{
+        lis.forEach((e)=>{e.style.display = 'none'});
+        document.querySelector('.searchpack').style.display = 'none';
+    }
+})
+
+document.querySelector('.navbar').addEventListener('resize', () => {
+    let disp = ham.style.display;
+    console.log(disp);
+    if (ham.style.display === 'none') {
+        lis.forEach((element) => {
+            element.style.display = 'block';
+        })
+        document.querySelector('.searchpack').style.display = 'block';
+    }
+})
 ham.addEventListener('click', function () {
     Array.from(lis).forEach((element) => {
         if (element.style.display === 'block') {
@@ -11,19 +32,10 @@ ham.addEventListener('click', function () {
             element.style.display = 'block';
             document.querySelector('.searchpack').style.display = 'block';
         }
-    })
-})
-btn.addEventListener('click', () => {
-    document.querySelector('.searchpack').style.display = 'none';
-    lis.forEach((element) => {
-        element.style.display = 'none';
-    })
+    });
 });
-Array.from(lis).forEach((e) => {
-    e.addEventListener('click', () => {
-        lis.forEach((e) => {
-            e.style.display = 'none';
-            document.querySelector('.searchpack').style.display = 'none';
-        })
-    })
-})
+if(window.innerWidth <= 991){
+    
+}
+
+
